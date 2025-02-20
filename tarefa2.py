4cf2781091885f3ed2c6f110bb6abfe75d8bcd54
Frascos09 <marcosdieguezrey9@gmail.com>
2025-02-20 14:00:54 +0100

    Add files via upload

--- tarefa2.py
+++ tarefa2.py
@@ -1,6 +1,10 @@
-beneHoxe = 10
-print(beneHoxe)
-beneHoxe = 22.22
-beneOnte = 33.33
-total = beneHoxe + beneOnte
-print(total)
+import math
+print (math.sqrt(25))
+moito = 9
+print (math.sqrt(moito))
+pacofiestas =23.8888
+ronaldo =45.8893
+romario =43.5667
+print (math.sqrt(pacofiestas))
+print(romario**2)
+print(math.pi)
