9e29c1d2c52499a8ddb86cc45799b2fef4a0ffa4
Frascos09 <marcosdieguezrey9@gmail.com>
2025-03-06 14:00:25 +0100

    Add files via upload

---
+++ tarefa6_cond.py
@@ -0,0 +1,10 @@
+persoa = input("Como te chamas?")
+
+if(persoa == "profe"):
+    print("bos dias   ", persoa)
+elif(persoa =="Marcos"):
+    print("Traballa un chisco")    
+else:
+        print("non sei quen es")
+elif(persoa =="Pedro")
+    print ("Gaviota")
