6e2bd9a41f45f17a37906a33dfa7f4e0f68080d7
Frascos09 <marcosdieguezrey9@gmail.com>
2025-03-10 16:38:57 +0100

    Add files via upload

---
+++ tarefa3.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.sqrt(33))
+
+
+paco= 26.262626
+pepe=89.898989
+pedrerol=33.33333
+print(math.sqrt(paco))
+print(math.pow(pepe,2))
+
+print(math.pi)
+print(round(math.pi))
+
+print(min(paco,pepe,pedrerol))
+import random
+print(random.randint(0,3))
