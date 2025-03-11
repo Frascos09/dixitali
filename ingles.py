0449a34695391a24749d91306d68be86f8a1ef0f
Frascos09 <marcosdieguezrey9@gmail.com>
2025-03-11 14:00:02 +0100

    Add files via upload

---
+++ ingles.py
@@ -0,0 +1,28 @@
+examen1 = input("como se dice manzana en ingles?")
+
+if(examen == "apple")
+ print("moi ben")
+else:
+        print("error")
+examen2 = input("como se dice perro en ingles?")
+if(examen == "dog")
+ print("moi ben")
+else:
+        print("error")
+examen3 = input("como se dice movil en ingles?")
+if(examen == "phone")
+ print("moi ben")
+else:
+        print("error")
+
+
+examen4 = input("como se dice coche en ingles?")
+if(examen == "car")
+ print("moi ben")
+else:
+        print("error")
+examen5 = input("como se dice gato en ingles?")
+if(examen == "cat")
+ print("moi ben")
+else:
+        print("error")
