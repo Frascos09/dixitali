f3aac7ee3fa337c166e4cc4d2b16a4c5be0d3159
Frascos09 <marcosdieguezrey9@gmail.com>
2025-03-17 16:48:03 +0100

    Add files via upload

---
+++ axenciamatrimonial.py
@@ -0,0 +1,11 @@
+print("Benvido ah First day")
+
+ollos = input("Como te gustaria que tuviera los ojos tu pareja?")
+pais = input("Cal é o país da túa vida?")
+deporte = input("Cal é o teu deporte preferido?")
+
+
+if(ollos == "negros"):
+    print("Neymar eo a tua mellor opción")
+if(ollos == "marrones"):
+    print("o te home eh Feranando")
