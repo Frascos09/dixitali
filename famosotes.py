fc1191dacb084fea2d20e282ac6c3ec954c57954
Frascos09 <marcosdieguezrey9@gmail.com>
2025-03-18 13:58:05 +0100

    Add files via upload

---
+++ famosotes.py
@@ -0,0 +1,17 @@
+print("Adivina el o la deportista")
+
+deporte = input("Que deporte practica?")
+
+
+if(deporte == "badminton"):
+    print("Carolina marin")
+if(deporte == "atletismo"):
+    print("Ana peleteiro")
+
+región = input("De que región?")
+
+
+if(región == "Andalucia"):
+    print("Carolina marin")
+if(región == "Galicia"):
+    print("Ana peleteiro")
